f0e78be81e575fa0c3aab86f38e70dbc00929211
Tom Guerney <tomguerney@gmail.com>
2024-05-30 22:15:35 +1000

    remove transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   site: 'https://thornburyshiatsu.com.au',
   integrations: [tailwind(), compress()],
 });
